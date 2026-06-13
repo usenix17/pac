3c383028197d02579be0733d4f7441fd5d72a959
usenix17 <usenix17@starnix.net>
2026-06-13 11:12:30 -0500

    feat: wire main entrypoint; pac update works end to end

---
+++ main.go
@@ -0,0 +1,13 @@
+// Command pac is one front door for pacman (official + aur-mirror) and flatpak.
+package main
+
+import (
+	"os"
+
+	"starnix.net/pac/internal/cli"
+	"starnix.net/pac/internal/run"
+)
+
+func main() {
+	os.Exit(cli.Run(os.Args[1:], run.Real{}, os.Stdout))
+}
